403b1a9f82361a936123daf9347918eaf85d7894
andrey <andrey@debian-BULLSEYE-live-builder-AMD64>
2023-08-08 16:23:06 +0300

    +

--- app/index.php
+++ app/index.php
@@ -1,3 +1,14 @@
 <?php 
 echo "test";
 phpinfo();
+
+/* тест соединения с бд
+try {
+    $dbh = new PDO('pgsql:host=db;port=5432;dbname=MY_DATABASE;user=MYSQL_USER;password=MYSQL_ROOT_PASSWORD');
+    echo "PDO connection object created";
+ }
+ catch(PDOException $e)
+ {
+       echo $e->getMessage();
+ }
+*/
